718a75670fcd0b5213b6b4e191c7c51bd52c93f3
Kondratiuk Oleksandr <kondratuiksasha5@gmail.com>
2025-05-10 13:11:52 +0300

    + implemented fighter preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,45 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+export function createFighterStats(fighter) {
+    const { name, health, attack, defense } = fighter;
+    const fighterProperties = { name, health, attack, defense };
+
+    const listElement = createElement({
+        tagName: 'ul',
+        className: 'fighter-preview___list'
+    });
+    Object.keys(fighterProperties).forEach(key =>
+        listElement.append(
+            createElement({
+                tagName: 'li',
+                className: '',
+                textContent: `${key}: ${fighterProperties[key]}`
+            })
+        )
+    );
+
+    return listElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    const fighterImage = createFighterImage(fighter);
+    const fighterStats = createFighterStats(fighter);
+
+    fighterElement.append(
+        position === 'right' ? fighterStats : fighterImage,
+        position === 'right' ? fighterImage : fighterStats
+    );
+
+    // todo: show fighter info (image, name, health, etc.)
+
+    return fighterElement;
 }
